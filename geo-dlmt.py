5d470b89aabe0e77f94e1df9ba771ee9799c59e0
Pilák Ottó <67331059+ottopl3@users.noreply.github.com>
2025-02-18 08:42:50 +0100

    Add files via upload

---
+++ geo-dlmt.py
@@ -0,0 +1,40 @@
+﻿import requests
+import csv
+from requests.structures import CaseInsensitiveDict
+
+def dlmtObject(id, jsonDump):
+	if len(jsonDump['features'])>0:
+		result = str(id) + "\t" + jsonDump['query']['text'] + "\t" + str(jsonDump['features'][0]['properties']['lat']) + "\t" + str(jsonDump['features'][0]['properties']['lon'])
+	else:
+		result = str(id) + "\t" + jsonDump['query']['text'] + "\t" + "na" + "\t" + "na"
+	return result
+	
+API_KEY = ""
+	
+with open("geokodolashoz.txt","r", encoding='utf-8') as file:
+    reader = csv.reader(file, delimiter='\t')
+    rows = [row for row in reader]
+
+i=0
+headers = CaseInsensitiveDict()
+headers["Accept"] = "application/json"
+f=open("result_dlmt.txt","w", encoding='utf-8')
+f.write("ID\tQuery\tlat\tlon\n")
+for row in rows:
+	id = row[0]
+	address = row[1]
+	print(address)
+	i=i+1
+
+	url = "https://api.geoapify.com/v1/geocode/search?text=" + address + "&filter=countrycode:hu&apiKey=" + API_KEY
+	print(url)
+
+	resp = requests.get(url, headers=headers)
+
+	print(resp.status_code)
+	geoResult = dlmtObject(id, resp.json())
+	print(geoResult)
+	
+	f.write(geoResult + "\n")
+f.close()
+	
---
+++ geo-json.py
@@ -0,0 +1,43 @@
+﻿import requests
+import json
+from requests.structures import CaseInsensitiveDict
+
+def geoObject(id, jsonDump):
+	if len(jsonDump['features'])>0:
+		result = '{"resultID":"' + str(id) + '","query":"' + jsonDump['query']['text'] + '", "lat":"' + str(jsonDump['features'][0]['properties']['lat']) + '", "lon":"' + str(jsonDump['features'][0]['properties']['lon']) + '"}'
+	else:
+		result = '{"resultID":"' + str(id) + '","query":"' + jsonDump['query']['text'] + '", "lat":"na", "lon":"na"}'
+	return result
+
+API_KEY = ""
+	
+data_file = open('geokodolashoz.json','r', encoding='utf-8')
+data = json.load(data_file)
+data_file.close()
+i=0
+headers = CaseInsensitiveDict()
+headers["Accept"] = "application/json"
+f=open("result_json.json","w", encoding='utf-8')
+f.write('[')
+for address in data:
+	print(address['CIM'])
+	i=i+1
+
+	url = "https://api.geoapify.com/v1/geocode/search?text=" + address['CIM'] + "&filter=countrycode:hu&apiKey=" + API_KEY
+	print(url)
+	
+
+	resp = requests.get(url, headers=headers)
+
+	print(resp.status_code)
+	geoResult = geoObject(address['ID'], resp.json())
+	print(geoResult)
+
+	f.write(geoResult)
+	if i<len(data):
+		f.write(',')
+f.write(']')
+f.close()
+
+
+	
